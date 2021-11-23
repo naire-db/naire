b1505fc3ba756aeed9533e41b2b3f4477ab17c88
karin0 <karin0@gmx.com>
2021-11-23 23:10:31 +0800

    user: add register

--- user/urls.py
+++ user/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('hello/', views.hello, name='hello'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('info/', views.info, name='info')
+    path('info/', views.info, name='info'),
+    path('register/', views.register, name='register'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
+from django.core.exceptions import BadRequest
 from django.views.decorators.http import require_safe
 from django.contrib import auth
 
-from common.rest import rest_ok, rest_fail, acquire_json, rest_data
+from common.rest import rest_ok, rest_fail, acquire_json, rest_data, rest
+from common.errors import ERR_DUPL_USERNAME, ERR_DUPL_EMAIL
 from common.log import logger
+
+from .models import User
 
 
 @require_safe
@@ -26,14 +30,42 @@ def login(request, data):
     username = str(data['username_or_email'])  # TODO: handle emails
     password = str(data['password'])
     user = auth.authenticate(request, username=username, password=password)
-    if user is not None:
-        auth.login(request, user)
-        logger.info(f'{request.session} logged in as {user}')
-        return rest_data(user.info())
-    return rest_fail()
+    if user is None:
+        return rest_fail()
+    auth.login(request, user)
+    logger.info(f'Logged: {user}')
+    return rest_data(user.info())
 
 
 @require_safe
 def logout(request):
     auth.logout(request)
     return rest_ok()
+
+
+@acquire_json
+def register(request, data):
+    username = str(data['username'])
+    email = str(data['email']).lower()
+    password = str(data['password'])
+    dname = str(data['dname'])
+    try:
+        User.objects.get(username=username)
+    except User.DoesNotExist:
+        pass
+    else:
+        return rest(ERR_DUPL_USERNAME)
+    try:
+        User.objects.get(email=email)
+    except User.DoesNotExist:
+        pass
+    else:
+        return rest(ERR_DUPL_EMAIL)
+    try:
+        user = User.objects.create_user(username=username, password=password, email=email, dname=dname)
+    except Exception as e:
+        # Shouldn't be reached from our validating frontend
+        logger.warning(f'Bad request caused {type(e).__name__} {e}')
+        raise BadRequest
+    logger.info(f'Registered: {user}')
+    return rest_data(user.info())
