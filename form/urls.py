dedd29baa9d2f636a2447106ca896c039cfe5007
karin0 <karin0@gmx.com>
2021-11-25 03:27:17 +0800

    form: create: impl

--- form/urls.py
+++ form/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('get_all/', views.get_all, name='get_all'),
+    path('create/', views.create, name='create'),
 ]
--- form/views.py
+++ form/views.py
@@ -1,8 +1,10 @@
-from django.views.decorators.http import require_safe
+from django.core.exceptions import BadRequest
+from django.views.decorators.http import require_safe, require_POST
 
-from common.rest import rest_data
 from common.deco import check_logged_in
-
+from common.log import logger
+from common.rest import rest_data, acquire_json, rest_ok
+from common.types import ensure_str, ensure_dict
 from .models import Form
 
 
@@ -11,3 +13,19 @@ from .models import Form
 def get_all(request):
     forms = Form.objects.filter(owner_user=request.user)
     return rest_data([dict(f.info(), resp_count=f.response_set.count()) for f in forms])
+
+
+@require_POST
+@check_logged_in
+@acquire_json
+def create(request, data):
+    title = ensure_str(data['title'])
+    body = ensure_dict(data['body'])
+    form = Form(title=title, body=body, owner_user=request.user)
+    try:
+        form.save()
+    except Exception as e:
+        # Abuser may provide a too long title
+        logger.warning(f'form.create: Bad request caused {type(e).__name__} {e}')
+        raise BadRequest
+    return rest_ok()
