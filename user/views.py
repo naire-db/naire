407d2053ba3b2892e138840edfb107dc2ae88f95
karin0 <karin0@gmx.com>
2021-11-26 02:17:10 +0800

    remove @require_POST where @acquire_json presents

--- user/views.py
+++ user/views.py
@@ -26,7 +26,6 @@ def info(request):
     return rest_fail()
 
 
-@require_POST
 @acquire_json
 def login(request, data):
     username = ensure_str(data['username_or_email'])  # TODO: handle emails
@@ -45,7 +44,6 @@ def logout(request):
     return rest_ok()
 
 
-@require_POST
 @acquire_json
 def register(request, data):
     username = ensure_str(data['username'])
