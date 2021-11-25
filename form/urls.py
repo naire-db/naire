b35b61a8b9454250a5aaab31cfdc1df415c59757
陈劲安 <79066012@buaa.edu.cn>
2021-11-25 14:38:05 +0800

    add app name in form/urls.py

--- form/urls.py
+++ form/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'form'
 urlpatterns = [
     path('get_all/', views.get_all, name='get_all'),
     path('create/', views.create, name='create'),
