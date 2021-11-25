0eca2abf5f5d80bbeb730a98ea2180188d3b9e2a
karin0 <karin0@gmx.com>
2021-11-25 16:56:10 +0800

    Revert "add app name in form/urls.py"
    This reverts commit b35b61a8b9454250a5aaab31cfdc1df415c59757.

--- form/urls.py
+++ form/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-app_name = 'form'
 urlpatterns = [
     path('get_all/', views.get_all, name='get_all'),
     path('create/', views.create, name='create'),
