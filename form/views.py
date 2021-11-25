9c89ca8af90ef0b0afb94b167a267dd094235f54
karin0 <karin0@gmx.com>
2021-11-26 04:32:19 +0800

    form: get_detail: limit the try block scope

--- form/views.py
+++ form/views.py
@@ -35,8 +35,11 @@ def create(request, data):
 def get_detail(request, data):
     fid = ensure_int(data['fid'])
     try:
-        # TODO: Check (future) permissions.
-        #  Maybe we shouldn't distinguish this from nonexistence in response code for security?
-        return rest_data(Form.objects.get(id=fid).detail())
+        form = Form.objects.get(id=fid)
     except Form.DoesNotExist:
         return rest_fail()
+
+    # TODO: Check (future) permissions.
+    #  Maybe we shouldn't distinguish permission denying from nonexistence in the result code for security?
+
+    return rest_data(form.detail())
