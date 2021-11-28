b7b33eef992973b92d5a9e71ac97e1eebfa529bc
Alvintan0712 <tjan2000@gmail.com>
2021-11-29 01:12:31 +0800

    attachment: upload: impl (progressing)

---
+++ tests/upload_file.py
@@ -0,0 +1,14 @@
+import requests
+
+with open('test.txt', 'rb') as f:
+    url = 'http://localhost:8000/attachment/upload_file/'
+    # url = 'https://httpbin.org/post'
+    files = {'file': f}
+    values = {'name': 'test.txt'}
+    resp = requests.post(url, files=files, data=values)
+    print(resp.text)
+    print(resp.status_code)
+    if resp.ok:
+        print('success')
+    else:
+        print('fail')
