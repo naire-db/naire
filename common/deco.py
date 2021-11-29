29ec7bc795fb59561ecf9d83d5d3a65d3594748e
karin0 <karin0@gmx.com>
2021-11-29 20:57:37 +0800

    common: generalize check_logged_in

--- common/deco.py
+++ common/deco.py
@@ -1,14 +1,16 @@
 import functools
 
+from django.http import HttpRequest
+
 from .rest import rest_msg
 from .errors import ERR_AUTH_REQUIRED
 
 
 def check_logged_in(view):
     @functools.wraps(view)
-    def wrapper(request):
+    def wrapper(request: HttpRequest, *args, **kwargs):
         if request.user.is_authenticated:
-            return view(request)
+            return view(request, *args, **kwargs)
         return rest_msg('login required', code=ERR_AUTH_REQUIRED)
 
     return wrapper
