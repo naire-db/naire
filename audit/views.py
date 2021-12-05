0860228daa190d0d9f77143f797a8b0407438a03
karin0 <karin0@gmx.com>
2021-12-05 22:58:15 +0800

    audit: views: rearrange imports

--- audit/views.py
+++ audit/views.py
@@ -1,11 +1,12 @@
 from django.views.decorators.http import require_safe
 
-from audit.models import Log
 from common.deco import check_logged_in
 from common.rest import rest_data
+
+from .models import Log
 
 
 @require_safe
 @check_logged_in
 def get_logs(request):
-    return rest_data([i.detail() for i in Log.objects.filter(session__user=request.user)])
+    return rest_data([i.detail() for i in Log.objects.filter(session__user=request.user).order_by('-id')])
