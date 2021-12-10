59378953573b6f06bd3b9b49573ec611342ec57d
Alvintan0712 <tjan2000@gmail.com>
2021-12-10 19:52:18 +0800

    tmpl: rename impl

--- template/urls.py
+++ template/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('remove/', views.remove, name='remove'),
     path('get_all/', views.get_all, name='get_all'),
     path('get_detail/', views.get_detail, name='get_detail'),
+    path('rename/', views.rename, name='rename'),
 ]
