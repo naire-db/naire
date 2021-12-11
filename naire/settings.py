b7d98d9299e227b1099de304115d775083435d0b
karin0 <karin0@gmx.com>
2021-12-11 21:37:17 +0800

    settings: apply secret midware earlier

--- naire/settings.py
+++ naire/settings.py
@@ -59,15 +59,18 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+]
+
+if is_secret:
+    MIDDLEWARE.append('common.secret.secret_middleware')
+
+MIDDLEWARE += [
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-if is_secret:
-    MIDDLEWARE.append('common.secret.secret_middleware')
 
 ROOT_URLCONF = 'naire.urls'
 
